bcf3473842c95d022d395fdd29d136a8b594e0ff
ADEYEMO OLUKAYODE <Jesselance1991@gmail.com>
2025-09-09 02:25:28 +0100

    updated python_calculator

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,4 +1,4 @@
 
 number1 = 10
 number2 = 5
-print(f"Addition: {number1 + number2}\nSubtraction: {number1 - number2}\nMultiplication:  {number1 * number2}\nDivision: {number1 / number2}")
+print(f"Addition of 10 and 5 is: {number1 + number2}\nSubtraction of 10 and 5 is: {number1 - number2}\nMultiplication of 10 and 5 is:  {number1 * number2}\nDivision of 10 and 5 i: {number1 / number2}")
