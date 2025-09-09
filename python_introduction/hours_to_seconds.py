12a7af35a5d12cd52963ef159f3dac827edc960d
ADEYEMO OLUKAYODE <Jesselance1991@gmail.com>
2025-09-09 07:26:06 +0100

    add new file

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,3 @@
+hours = 2
+number_of_seconds_in_a_given_hour = (2*60*60)
+print("2 hours is", number_of_seconds_in_a_given_hour,"s")
---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,4 @@
+length = 10
+width =  5
+area  = (length*width)
+print("The Area of the Rectangle is:", area)
